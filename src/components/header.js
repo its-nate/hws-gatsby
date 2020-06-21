e5f8c42f20b8c8b78d26be8a1dd34b837a7c25ef
Nate Micinski <nmicinski@gmail.com>
2020-06-20 20:18:55 -0700

    add images, navbar displays logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,54 @@
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import React from "react"
+import Img from "gatsby-image"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    {/* Nav */}
-    <ul id="slide-out" class="sidenav sidenav-fixed">
-      <Link to="/">
-        <img
-          class="logo-nav"
-          src="assets/hws_logo_small.png"
-          alt="Holistic Web Solutions Logo"
-        />
-      </Link>
-      <li>
-        <Link to="/services">Services</Link>
-      </li>
-      <li>
-        <Link to="/clients">Clients</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "hws_logo_small.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-    <nav>
-      <a href="#" data-target="slide-out" class="sidenav-trigger">
-        <i class="material-icons">menu</i>
-      </a>
-      <Link to="/">
-        {" "}
-        <h1 class="nav-header center-align">Holistic Web Solutions</h1>
-      </Link>
-    </nav>
-  </header>
-)
+  return (
+    <header>
+      {/* Nav */}
+      <ul id="slide-out" class="sidenav sidenav-fixed">
+        <Link to="/">
+          <Img
+            className="logo-nav"
+            fluid={data.placeholderImage.childImageSharp.fluid}
+            alt="Holistic Web Solutions"
+            loading="eager"
+          />
+        </Link>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <nav>
+        <a href="#" data-target="slide-out" class="sidenav-trigger">
+          <i class="material-icons">menu</i>
+        </a>
+        <Link to="/">
+          {" "}
+          <h1 class="nav-header center-align">Holistic Web Solutions</h1>
+        </Link>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
