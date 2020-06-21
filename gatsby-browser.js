9270e4354183c008150ab8665319e3f6520bb32d
Nate Micinski <nmicinski@gmail.com>
2020-06-20 20:02:04 -0700

    add materialize css, custom css, font files, and header component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import "materialize-css/dist/css/materialize.css"
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,38 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    {/* Nav */}
+    <ul id="slide-out" class="sidenav sidenav-fixed">
+      <Link to="/">
+        <img
+          class="logo-nav"
+          src="assets/hws_logo_small.png"
+          alt="Holistic Web Solutions Logo"
+        />
+      </Link>
+      <li>
+        <Link to="/services">Services</Link>
+      </li>
+      <li>
+        <Link to="/clients">Clients</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+
+    <nav>
+      <a href="#" data-target="slide-out" class="sidenav-trigger">
+        <i class="material-icons">menu</i>
+      </a>
+      <Link to="/">
+        {" "}
+        <h1 class="nav-header center-align">Holistic Web Solutions</h1>
+      </Link>
+    </nav>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
